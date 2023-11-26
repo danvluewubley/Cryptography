2f3556068bc34facb0b0a9bd85d8cd729a3a242a
Daniel <danielw72908@gmail.com>
2023-11-26 13:37:29 -0500

    allow user to input key and text

--- 2x2_matrix.py
+++ 2x2_matrix.py
@@ -1,7 +1,9 @@
 import numpy as np
 
-key = "DDCF"
-plain_text = "HELPS"
+key = input("Key:" )
+plain_text = input("Plain Text: ").replace(" ", "")
+cipher_text = input("Cipher Text: ").replace(" ", "")
+print(type(cipher_text))
 
 # changes the key string (4 chars) into a matrix
 def key_to_matrix(key):
@@ -11,11 +13,9 @@ def key_to_matrix(key):
     index = ord(key[i]) - 97
     new_key.append(index)
   key_matrix = [[new_key[0], new_key[1]],[new_key[2], new_key[3]]]
-  print("Matrix for the key:")
-  print(key_matrix)
   return key_matrix
 
-def plain_text_to_matrix(plain_text):
+def text_to_matrix(plain_text):
   plain_text = [*plain_text.lower()]
   colums = len(plain_text)/2
   if colums % 2 == 0:
@@ -34,12 +34,31 @@ def plain_text_to_matrix(plain_text):
   for c in range(int(colums)):
     for r in range(2):
         plain_text_matrix[r][c] = new_text[n]
-        print(plain_text_matrix)
         n+=1
-  print("Matrix for the plain text:")
-  print(plain_text_matrix.tolist())
   return plain_text_matrix.tolist()
   
+def cipher_text_to_matrix(cipher_text):
+  cipher_text = [*cipher_text.lower()]
+  colums = len(cipher_text)/2
+  if colums % 2 == 0:
+    pass
+  else:
+    cipher_text.append("z")
+    colums+=0.5
+
+  new_text = []
+  for i in range(len(cipher_text)):
+    index = ord(cipher_text[i]) - 97
+    new_text.append(index)
+  
+  cipher_text_matrix = np.zeros( (2, int(colums)) )
+  n = 0
+  for c in range(int(colums)):
+    for r in range(2):
+        cipher_text_matrix[r][c] = new_text[n]
+        n+=1
+  return cipher_text_matrix.tolist()
+
 def plain_text_to_cipher_text(plain_text, key):
   cipher_text_array = []
   for n in range(len(plain_text[0])):
@@ -47,16 +66,18 @@ def plain_text_to_cipher_text(plain_text, key):
       result = (key[r][0] * plain_text[0][n] + key[r][1] * plain_text[1][n]) % 26 + 97
       cipher_text_array.append(chr(int(result)).upper())
   cipher_text = ""
-  print("Cipher Text: " + cipher_text.join(cipher_text_array))
   return cipher_text_array
 
 # utilizes the modular multiplicative inverse properity to identify the congruent determinant
 def inverse_modular(modular, a):
   n = 0
-  for n in range(20):
-    value = a*n
-    if value % 26 == 1:
-      return n
+  if a % 2 == 0:
+    pass
+  else:
+    for n in range(100):
+      value = a*n
+      if value % 26 == 1:
+        return n
 
 # inverts the matrix
 def matrix_inverter(matrix):
@@ -65,7 +86,6 @@ def matrix_inverter(matrix):
   
   adjoint = [[matrix[1][1], -matrix[0][1]],[-matrix[1][0],matrix[0][0]]]
   inverse_matrix = [[(adjoint[0][0]*determinant)%26,(adjoint[0][1]*determinant)%26],[(adjoint[1][0]*determinant)%26,(adjoint[1][1]*determinant)%26]]
-  print(inverse_matrix)
   return inverse_matrix
 
 def cipher_text_to_plain_text(cipher_text, key):
@@ -79,31 +99,28 @@ def cipher_text_to_plain_text(cipher_text, key):
     index = ord(cipher_text[n])-65
     cipher_text_array.append(index)
 
-  print(cipher_text_array)
-  
   cipher_text_matrix = np.zeros( (2, int(colums)) )
   n = 0
   for c in range(int(colums)):
     for r in range(2):
         cipher_text_matrix[r][c] = cipher_text_array[n]
-        print(cipher_text_matrix)
         n+=1
-  print("Matrix for the plain text:")
-  print(cipher_text_matrix)
 
   inverse_key_matrix = matrix_inverter(key)
   for n in range(int(colums)):
     for r in range(2):
       result = (inverse_key_matrix[r][0] * cipher_text_matrix[0][n] + inverse_key_matrix[r][1] * cipher_text_matrix[1][n]) % 26 + 97
       plain_text_array.append(chr(int(result)).upper())
-      print(result)
   plain_text = ""
-  print("Cipher Text: " + plain_text.join(plain_text_array))
   return plain_text_array
 
-
-
-plain_text_matrix = plain_text_to_matrix(plain_text)
 key_matrix = key_to_matrix(key)
-cipher_text_matrix = plain_text_to_cipher_text(plain_text_matrix, key_matrix)
-cipher_text_to_plain_text(cipher_text_matrix, key_matrix)
+
+if cipher_text == "":
+  plain_text_matrix = text_to_matrix(plain_text)
+  cipher_text = plain_text_to_cipher_text(plain_text_matrix, key_matrix)
+  print(cipher_text)
+if plain_text == "":
+  plain_text_matrix = text_to_matrix(cipher_text)
+  plain_text = cipher_text_to_plain_text(cipher_text, key_matrix)
+  print(plain_text)
